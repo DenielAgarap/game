e1b24e12456751dd34e787200d4466f3c8f545ff
DJ Gamer <djgamer@DJs-MacBook-Pro.local>
2024-12-02 10:06:54 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <title>MAssO Dashboard</title>
+    <link rel="stylesheet" href="styleListing1.css" />
+    <!-- Font Awesome Cdn Link -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  </head>
+  <body>
+    <div class="container">
+    <nav>
+  <ul>
+    <li class="logo">
+      <img src="logo.png" alt="Logo">
+      <span class="nav-item">MAssO DashBoard</span>
+    </li>
+    <li>
+      <a href="adminDashboard.php">
+        <i class="fas fa-home"></i>
+        <span class="nav-item">Home</span>
+      </a>
+    </li> 
+    <li>
+      <a href="#">
+        <i class="fas fa-tasks"></i>
+        <span class="nav-item">Listings</span>
+      </a>
+    </li>
+    <li>
+      <a href="adminSearch.php">
+        <i class="fas fa-search"></i>
+        <span class="nav-item">Search</span>
+      </a>
+    </li>
+    <li>
+      <a href="logout.php" class="logout">
+        <i class="fas fa-sign-out-alt"></i>
+        <span class="nav-item">Log out</span>
+      </a>
+    </li>
+  </ul>
+</nav>
+<!--MAIN CONTENT-->
+
+</body>
+</html>
